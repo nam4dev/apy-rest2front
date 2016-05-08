0677b8fd75d9db486e462122c0394b3de9e2327c
Namgyal Brisson <namat4css@gmail.com>
2016-05-08 14:07:15 +0200

    Refactor App structure

--- app/components/apy-frontend/core.js
+++ app/components/apy-frontend/core.js
@@ -1,4 +1,39 @@
+/**
+ *  MIT License
+ *
+ *  This project is a small automated frontend application based on a REST API schema.
+ *  It tries to implement a generic data binding upon a REST API system.
+ *  For now, python-eve REST API framework has been integrated to Apy Frontend.
+ *  For UI components (data representation & bindings), AngularJs is used.
+ *  Anyhow, the framework is intended to be plugged to any UI or Backend framework...
+ *
+ *  Copyright (C) ${YEAR}  (${USER}) Namgyal Brisson
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
 
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *
+ *  `apy-frontend`  Copyright (C) ${YEAR}  (${USER}) Namgyal Brisson.
+ *
+ *  """
+ *  Write here what the module does...
+ *
+ *  """
+ */
 (function ($window) {'use strict';
 
     /**
--- app/components/apy-frontend/frameworks/front/angular/view.js
+++ app/components/apy-frontend/frameworks/front/angular/view.js
@@ -1,3 +1,39 @@
+/**
+ *  MIT License
+ *
+ *  This project is a small automated frontend application based on a REST API schema.
+ *  It tries to implement a generic data binding upon a REST API system.
+ *  For now, python-eve REST API framework has been integrated to Apy Frontend.
+ *  For UI components (data representation & bindings), AngularJs is used.
+ *  Anyhow, the framework is intended to be plugged to any UI or Backend framework...
+ *
+ *  Copyright (C) ${YEAR}  (${USER}) Namgyal Brisson
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *
+ *  `apy-frontend`  Copyright (C) ${YEAR}  (${USER}) Namgyal Brisson.
+ *
+ *  """
+ *  Write here what the module does...
+ *
+ *  """
+ */
 (function ($angular) {'use strict';
 
     $angular.module('apy.view', ['ngRoute'])
@@ -23,12 +59,10 @@
 
                     $scope.displayChoices = function () {
                         $scope.displaySchemaNames = true;
-                        console.log('FILS DE PUTA')
                     };
 
                     $scope.hideChoices = function () {
                         $scope.displaySchemaNames = false;
-                        console.log('FILS DE PUTA MADRE')
                     };
 
                     $scope.setFile = function (field, file) {
@@ -110,7 +144,6 @@
 
                 collection.fetch()
                     .then(function (_) {
-                        $log.debug('[views.js] Collection', collection);
                         $scope.$apply();
                     }).catch(function (error) {
                         $log.error(error);
@@ -139,8 +172,6 @@
                     // TODO: Add confirmation box before deleting
                     var ok = confirm("Would you really like to delete " + collection.count() + " listed resources ?");
                     if(ok) {
-                        $log.debug("CONFIRMED");
-                        $log.debug("About to delete", collection.count(), "resources...");
                         collection.delete();
 
                     }
@@ -179,9 +210,6 @@
                             $scope.$apply();
                         });
                     }
-                    // TODO: Resource must handle a save method
-                    // TODO: Resource must used here instead of Collection
-                    // TODO: Collection shall only be used for macro/overall actions
                 };
 
                 $scope.delete = function (resource) {
