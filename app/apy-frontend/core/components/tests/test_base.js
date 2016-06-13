54be0d0cc84e6d7cbaeb30136b4c360b87ea5281
Namgyal Brisson <namat4css@gmail.com>
2016-06-13 23:55:55 +0200

    [UT] Base Component

--- app/apy-frontend/core/components/tests/test_base.js
+++ app/apy-frontend/core/components/tests/test_base.js
@@ -30,7 +30,59 @@
  *  `apy-frontend`  Copyright (C) 2016  (apy) Namgyal Brisson.
  *
  *  """
- *  Write here what the module does...
+ *  Component Base UTs
  *
  *  """
  */
+
+describe("Component.Base unit tests", function() {
+
+    var _createBaseComponent = function (type, value) {
+        var component = new ApyComponentMixin();
+        component.init({$log: console}, "Component.Base.test", value, type);
+        return component;
+    };
+
+    it("[add.a.child] Inner $components Array shall contains the child", function () {
+        var child = "A string";
+        var component = _createBaseComponent();
+        component.add(child);
+        expect(component.$components.length).toEqual(1);
+        expect(component.$components[0]).toEqual(child);
+    });
+
+    it("[prepend.a.child] Inner $components Array shall contains the child at index 0", function () {
+        var child = "A string";
+        var prependedChild = "Another string";
+        var component = _createBaseComponent();
+        component.add(child);
+        expect(component.$components[0]).toEqual(child);
+        component.prepend(prependedChild);
+        expect(component.$components.length).toEqual(2);
+        expect(component.$components[0]).toEqual(prependedChild);
+        expect(component.$components[1]).toEqual(child);
+    });
+
+    it("[count] Shall return inner $components Array length", function () {
+        var children = [1, 2, 3, 4, 5, 6, 7, 8];
+        var component = _createBaseComponent();
+        children.forEach(function (child) {
+            component.add(child);
+        });
+        expect(component.count()).toEqual(children.length);
+        expect(component.count()).toEqual(component.$components.length);
+        expect(component.$components).toEqual(children);
+    });
+
+    it("[remove] Shall remove given child", function () {
+        var children = [1, 2, 3, 4, 5, 6, 7, 8];
+        var component = _createBaseComponent();
+        children.forEach(function (child) {
+            component.add(child);
+        });
+        expect(component.count()).toEqual(8);
+        component.remove(children[2]); // value: 3
+        expect(component.count()).toEqual(7);
+        expect(component.getChild(2)).toEqual(children[3]);
+    });
+});
