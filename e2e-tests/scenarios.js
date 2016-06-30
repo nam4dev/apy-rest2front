7de35a424ec3bd399e37c5a941553018f101d24b
Namgyal Brisson <namat4css@gmail.com>
2016-06-30 23:04:34 +0200

    Enable End-to-end (E2E) tests & add some

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -2,41 +2,59 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
-describe('my app', function() {
+describe('[Apy Frontend]', function() {
 
-
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
-  });
-
-
-  describe('view1', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view1');
+    it('should automatically redirect to /index when location hash/fragment is empty', function() {
+        browser.get('app/');
+        expect(browser.getLocationAbsUrl()).toMatch("/index");
     });
 
+    describe('[Apy-eve-demo] Jobs View Integration Tests', function() {
+        beforeEach(function() {
+            browser.get('app/#/jobs');
+        });
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+        it('should render Jobs items when user navigates to /jobs', function() {
+            element.all(by.css('.table tbody tr')).getSize().then(function (size) {
+                expect(size.length).toBeGreaterThan(0);
+            });
+        });
+
+        it('should prompt a confirmation window before deleting Jobs items when user clicks onto the `global` trash icon', function() {
+            browser.findElement(by.css('i#gDelete')).click().then(function () {
+                browser.switchTo().alert().dismiss();
+            });
+        });
     });
 
-  });
+    describe('[Apy-eve-demo] Work Steps View Integration Tests', function() {
+        beforeEach(function() {
+            browser.get('app/#/work_steps');
+        });
 
+        it('should create Work step items', function() {
+            element.all(by.css('.table tbody tr')).getSize().then(function (size) {
+                expect(size.length).toBeGreaterThan(0);
+            });
+        });
 
-  describe('view2', function() {
+        it('should render Work step items when user navigates to /work_steps', function() {
+            element.all(by.css('.table tbody tr')).getSize().then(function (size) {
+                expect(size.length).toBeGreaterThan(0);
+            });
+        });
 
-    beforeEach(function() {
-      browser.get('index.html#/view2');
+        it('should delete all items when user clicks onto the `global` trash icon then `yes`', function() {
+            browser.findElement(by.css('i#gDelete')).click();
+            browser.switchTo().alert().accept();
+            var asyncExec = function () {
+                element.all(by.css('.table tbody tr')).getSize().then(function (size) {
+                    expect(size.length).toEqual(0);
+                    clearTimeout(id);
+                });
+            };
+            //  Need to wait for all items to be deleted (async)
+            var id = setTimeout(asyncExec, 1000);
+        });
     });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
