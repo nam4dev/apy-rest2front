a0c2f6656c0ba370bdb576570242ae9fd7475678
Namgyal Brisson <namat4css@gmail.com>
2016-08-07 00:28:05 +0200

    Implement ApyModal Provider

--- app/apy-frontend/integration/angular/app.js
+++ app/apy-frontend/integration/angular/app.js
@@ -37,6 +37,174 @@
 /* istanbul ignore next */
 (function (angular) {'use strict';
 
+    var messagesBasedTemplate = ' \
+        <div class="modal-header btn-{{ widgetClass || \'info\' }}"> \
+            <h3 class="modal-title">  \
+                 <span class="text-capitalize" >  \
+                     <strong>{{ title }}</strong>  \
+                 </span>  \
+            </h3>  \
+        </div>  \
+        <div class="modal-body">  \
+            <p ng-if="message">  \
+                <strong><i>{{ message }}</i></strong>  \
+            </p> \
+            <div class="alert alert-{{ widgetClass || \'info\' }}" role="alert" ng-if="messages && !messages.push" > \
+                <p ng-repeat="(title, message) in messages track by $index"> \
+                    {{ title }} => <strong>{{ message }}</strong> \
+                </p> \
+            </div> \
+            <div class="alert alert-{{ widgetClass || \'info\' }}" role="alert" ng-if="messages && messages.push && messages.length" > \
+                <ul class="list-group" ng-if="asList"> \
+                    <li class="list-group-item" ng-repeat="message in messages track by $index"> \
+                        <span class="badge" ng-if="numberedList">{{ $index + 1 }}</span> \
+                        <strong>{{ message }}</strong> \
+                    </li> \
+                </ul> \
+                <p ng-if="!asList" ng-repeat="message in messages track by $index"> \
+                    <strong>{{ message }}</strong> \
+                </p> \
+            </div> \
+        </div> \
+        <div class="modal-footer"> \
+            <button class="btn btn-{{ okWidgetClass || \'info\' }}" type="button" ng-click="ok()" ng-if="ok">{{ okBtnName || "Ok" }}</button> \
+            <button class="btn btn-{{ cancelWidgetClass || \'default\' }}" type="button" ng-click="cancel()" ng-if="cancel">{{ cancelBtnName || "Cancel" }}</button> \
+        </div> \
+    ';
+
+    var ApyModalProxy = function ($rootScope, $modal) {
+        var instances = [];
+        var currentInstance;
+
+        return {
+            cancel: function () {
+                currentInstance && currentInstance.close(0);
+            },
+            cancelAll: function () {
+                instances.forEach(function (instance) {
+                    instance && instance.close(0);
+                });
+            },
+            base: function base(config) {
+
+                function isFunc(callback) {
+                    return callback && isFunction(callback);
+                }
+
+                function executeIfPossible(callback) {
+                    isFunc(callback) && callback();
+                }
+
+                function callbacksWrapper(isOk) {
+                    if(isOk) {
+                        executeIfPossible(okCallback);
+                    }
+                    else {
+                        executeIfPossible(cancelCallback);
+                    }
+                }
+
+                var $instance;
+                var okCallback = config.okCallback;
+                var cancelCallback = config.cancelCallback;
+                var $scope = $rootScope.$new();
+
+                var options = {
+                    animation: config.animation || true,
+                    template: messagesBasedTemplate,
+                    controllerAs: 'ModalCtrl',
+                    scope: $scope
+                };
+
+                $scope.title = config.title;
+                $scope.asList = config.asList;
+                $scope.message = config.message;
+                $scope.messages = config.messages;
+                $scope.okBtnName = config.okBtnName;
+                $scope.numberedList = config.numberedList;
+                $scope.cancelBtnName = config.cancelBtnName;
+                $scope.widgetClass = config.widgetClass;
+                $scope.okWidgetClass = config.okWidgetClass || config.widgetClass;
+                $scope.cancelWidgetClass = config.cancelWidgetClass || config.widgetClass;
+                // Define default OK button behavior
+                $scope.ok = function () {
+                    $instance.close(1);
+                };
+                if(isFunc(cancelCallback)) {
+                    $scope.cancel = function () {
+                        $instance.close(0);
+                    };
+                }
+                currentInstance = $instance = $modal.open(options);
+                instances.push($instance);
+                $instance.result.then(callbacksWrapper);
+            },
+            info: function info(config) {
+                this.base(config);
+            },
+            warn: function warn(config) {
+                var cls = 'warning';
+                this.base({
+                    asList: true,
+                    widgetClass: cls,
+                    okWidgetClass: cls,
+                    numberedList: true,
+                    title: config.title,
+                    message: config.message,
+                    messages: config.messages,
+                    okCallback: config.okCallback,
+                    cancelCallback: config.cancelCallback
+                })
+            },
+            error: function error(e, okCallback) {
+                var cls = 'danger';
+                this.base({
+                    title: e.title,
+                    messages: e.messages,
+                    okCallback: okCallback,
+                    cancelCallback: config.cancelCallback,
+                    widgetClass: cls,
+                    okWidgetClass: cls
+                })
+            },
+            errors: function errors(errorList) {
+                var cls = 'danger';
+                var messages = [];
+                errorList.forEach(function (error) {
+                    console.log('Error', error);
+                    var iter;
+                    if(isObject(error.messages)) {
+                        iter = [];
+                        Object.keys(error.messages).forEach(function (k) {
+                            iter.push(k + ' => ' + error.messages[k]);
+                        });
+                    }
+                    else {
+                        iter = error.messages;
+                    }
+                    messages.push(error.title);
+                    iter.forEach(function (value) {
+                        messages.push(value);
+                    })
+                });
+                this.base({
+                    title: "Errors",
+                    messages: messages,
+                    okCallback: config.okCallback,
+                    cancelCallback: config.cancelCallback,
+                    widgetClass: cls,
+                    okWidgetClass: cls
+                })
+            },
+            success: function success(config) {
+                var cls = 'success';
+                config.widgetClass = cls;
+                config.okWidgetClass = cls;
+                this.base(config);
+            }
+        }
+    };
+
     var config = {
         endpoint: 'http://localhost:8002/',
         schemasEndpointName: 'schemas',
@@ -70,6 +238,13 @@
         'apy-frontend.view',
         'apy-frontend.version'
     ])
+        .provider("apyModal", function apyModalProvider () {
+            this.$get = function apyModalFactory () {
+                var $injector = angular.injector(['ng', 'ui.bootstrap']),
+                    $uibModal = $injector.get('$uibModal');
+                return new ApyModalProxy($injector.get('$rootScope'), $uibModal);
+            };
+        })
         .provider("apy", function apyProvider () {
             this.$get = function apyFactory () {
                 var $injector = angular.injector(['ng', 'ngFileUpload']),
@@ -112,7 +287,7 @@
         }]);
 
     if(config.$auth()) {
-        app.controller('apyLoginCtrl', ['$scope', 'apy', function ($scope, apyProvider) {
+        app.controller('apyLoginCtrl', ['$scope', 'apy', 'apyModal', function ($scope, apyProvider, apyModalProvider) {
             if(!$scope.credentials ||
                 !$scope.credentials.username ||
                 !$scope.credentials.password) {
@@ -124,7 +299,8 @@
 
             $scope.help = function (event) {
                 event.preventDefault();
-                console.log({
+
+                apyModalProvider.info({
                     title: "Restricted Area",
                     messages: [
                         "This area cannot be accessed freely.",
@@ -145,7 +321,7 @@
                 }
 
                 if(errors.length) {
-                    console.error(new ApyEveError(errors));
+                    apyModalProvider.error(new ApyEveError(errors));
                 }
                 else {
                     apyProvider.authenticate($scope.credentials)
@@ -153,7 +329,7 @@
                             window.location.reload();
                         })
                         .catch(function (error) {
-                            console.error(error);
+                            apyModalProvider.error(error);
                         });
                 }
             };
