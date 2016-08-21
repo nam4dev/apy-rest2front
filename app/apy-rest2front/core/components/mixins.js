5883d18282be715eae86c6953bea65d33a6e2da0
Namgyal Brisson <namat4css@gmail.com>
2016-08-22 00:11:19 +0200

    Renamed cor/components/common.js to mixins.js

--- app/apy-rest2front/core/components/mixins.js
+++ app/apy-rest2front/core/components/mixins.js
@@ -30,29 +30,31 @@
  *
  *  `apy-rest2front`  Copyright (C) 2016  (apy) Namgyal Brisson.
  */
-(function ( $apy ) {
-
+(function($apy) {
     /**
      *  RequestMixin offer a common interface to access
-     *  low-level resources
+     *  low-level network resources
      *
      * @mixin apy.components.RequestMixin
      */
-    $apy.components.RequestMixin = (function RequestMixin() { // Registering mixin globally
+    $apy.components.RequestMixin = (function RequestMixin() {
+ // Registering mixin globally
 
         /**
+         * Set `Authorization` HTTP Header (if available)
          *
-         * @param headers
-         * @returns {Object}
          * @memberOf apy.components.RequestMixin
          *
+         * @param {Object} headers (optional) HTTP Headers
+         *
+         * @return {Object} HTTP Headers with `Authorization` (if available)
          */
         function $authHeaders(headers) {
             var service = this.$service || this;
             headers = headers || {
                 'Content-Type': 'application/json'
             };
-            if(service && service.$tokenInfo && service.$tokenInfo.access_token) {
+            if (service && service.$tokenInfo && service.$tokenInfo.access_token) {
                 var authToken = '';
                 var type = service.$tokenInfo.token_type;
                 authToken += (type ? type : 'Bearer') + ' ' + service.$tokenInfo.access_token;
@@ -62,13 +64,18 @@
         }
 
         /**
+         * Method to manage HTTP requests
          *
-         * @param request
-         * @returns {*}
+         * Authentication management (Authorization & Bearer)
+         *
          * @memberOf apy.components.RequestMixin
+         *
+         * @param {Object} request The Request object
+         *
+         * @return {Promise} Asynchronous call
          */
         function $access(request) {
-            if(!this.$request) {
+            if (!this.$request) {
                 this.$request = (this.$schema && this.$schema.$hasMedia) ?
                     this.$upload.upload : this.$http;
             }
@@ -80,19 +87,20 @@
         /**
          * Common Method for components which need to request on network
          *
-         * @param uri: The URI to contact
-         * @param method: The HTTP method Verb (GET, POST, PATCH, ...)
-         *
-         * @returns {Promise} Asynchronous
          * @memberOf apy.components.RequestMixin
+         *
+         * @param {string} uri The URI to contact
+         * @param {string} method The HTTP method Verb (GET, POST, PATCH, ...)
+         *
+         * @return {Promise} Asynchronous call
          */
-        function createRequest (uri, method) {
+        function createRequest(uri, method) {
             var self = this;
-            var setConfig = function () {
+            var setConfig = function() {
                 request.url += '/' + self._id;
                 request.headers['If-Match'] = self._etag;
             };
-            if(method === 'PATCH' && !self._id) {
+            if (method === 'PATCH' && !self._id) {
                 method = 'POST';
             }
             method = method || 'POST';
@@ -115,14 +123,14 @@
             default :
                 break;
             }
-            return new Promise(function (resolve, reject) {
-                return self.$access(request).then(resolve, function (error) {
-                    return reject(new $apy.EveHTTPError(error));
+            return new Promise(function(resolve, reject) {
+                return self.$access(request).then(resolve, function(error) {
+                    return reject(new $apy.errors.EveHTTPError(error));
                 });
             });
         }
 
-        return function () {
+        return function() {
             this.$access = $access;
             this.$authHeaders = $authHeaders;
             this.createRequest = createRequest;
@@ -135,35 +143,38 @@
      *
      * @mixin apy.components.CompositeMixin
      */
-    $apy.components.CompositeMixin =  (function CompositeMixin() { // Registering mixin globally
+    $apy.components.CompositeMixin = (function CompositeMixin() {
+// Registering mixin globally
 
         /**
          * Common Method for composite container components (Resource, List)
          *
-         * @param type: The Component type
-         * @param name: The Component name
-         * @param schema: The Component (data-)schema
-         * @param value: (optional) The component value
-         * @param endpoint: The Application global endpoint
-         * @param append: If true, the created component instance is appended to `this`
-         *
-         * @returns {*} A component instance
          * @memberOf apy.components.CompositeMixin
+         *
+         * @param {string} type The Component type
+         * @param {string} name The Component name
+         * @param {Object} schema The Component (data-)schema
+         * @param {*} value (optional) The component value
+         * @param {string} endpoint The Application global endpoint
+         * @param {boolean} append If true, the created component instance is appended to `this`
+         *
+         * @return {*} A component instance
          */
-        function createField (type, name, schema, value, endpoint, append) {
+        function createField(type, name, schema, value, endpoint, append) {
             var fieldObj;
             // true by default if not specifically set to false
+            schema = schema || {};
             append = append === false ? append : true;
             endpoint = endpoint || this.$endpoint;
-            switch(type) {
+            switch (type) {
             case $apy.helpers.$TYPES.LIST:
                 fieldObj = new $apy.components.fields.List(this.$service, name, schema, value, this.$states, endpoint);
                 break;
             case $apy.helpers.$TYPES.DICT:
                 fieldObj = new $apy.components.fields.Nested(this.$service, name, schema.schema, value, this.$states, null, $apy.helpers.$TYPES.RESOURCE);
-                //if(!schema.schema) {
+                // if(!schema.schema) {
                 //    fieldObj.add(fieldObj.createPolyField(undefined, value, name));
-                //}
+                // }
                 break;
             case $apy.helpers.$TYPES.POINT:
                 fieldObj = new $apy.components.fields.geo.Point(this.$service, name, schema, value, this.$states, endpoint);
@@ -199,22 +210,25 @@
                 break;
             }
             fieldObj.setParent(this);
-            if(append) {
+            if (append) {
                 this.add(fieldObj);
             }
             return fieldObj;
         }
 
         /**
-         * Private `template method`
+         * `template method`
          * Groups Composite (recursive) logic.
          *
-         * @param resource: (optional) A Resource Object ({})
          * @memberOf apy.components.CompositeMixin
+         *
+         * @param {Object} resource (optional) A Resource object
+         *
+         * @return {apy.components.CompositeMixin} `this`
          */
-        function _load (resource) {
+        function _load(resource) {
             for (var field in this.$schema) {
-                if(!this.$schema.hasOwnProperty(field) ||
+                if (!this.$schema.hasOwnProperty(field) ||
                     this.continue(field) ||
                     this.continue(field, '$')) {
                     continue;
@@ -223,7 +237,7 @@
                 try {
                     var type = schema.type;
                 }
-                catch(e) {
+                catch (e) {
                     continue;
                 }
                 var endpoint = this.$endpointBase;
@@ -238,16 +252,18 @@
          *
          * Groups Composite (recursive) logic.
          *
-         * @param resource: (optional) A Resource Object ({})
-         * @returns {this}
          * @memberOf apy.components.CompositeMixin
+         *
+         * @param {Object} resource (optional) A Resource object
+         *
+         * @return {apy.components.CompositeMixin} `this`
          */
-        function load (resource) {
+        function load(resource) {
             var self = this;
             resource = resource || {};
             var resourceObj = {};
-            Object.keys(Object.assign(resource)).forEach(function (k) {
-                if(self.continue(k)) {
+            Object.keys(Object.assign(resource)).forEach(function(k) {
+                if (self.continue(k)) {
                     self[k] = resource[k];
                 }
                 else {
@@ -255,11 +271,11 @@
                 }
             });
             this._load(resourceObj);
-            this.$components.sort(function (one, two) {
-                if ( one.$name < two.$name ) {
+            this.$components.sort(function(one, two) {
+                if (one.$name < two.$name) {
                     return 1;
                 }
-                if ( one.$name > two.$name ) {
+                if (one.$name > two.$name) {
                     return -1;
                 }
                 return 0;
@@ -272,14 +288,15 @@
          * Allow the component to restore its initial state
          * (based on its `$memo` attribute)
          *
-         * @returns {this}
          * @memberOf apy.components.CompositeMixin
+         *
+         * @return {apy.components.CompositeMixin} `this`
          */
-        function reset () {
-            this.$components.forEach(function (comp) {
+        function reset() {
+            this.$components.forEach(function(comp) {
                 comp.reset();
             });
-            if(this.$selfUpdated) {
+            if (this.$selfUpdated) {
                 this.$selfUpdated = false;
             }
             return this;
@@ -290,9 +307,11 @@
          * Trick here is to use JS native mechanism to iterate over the object.
          * If it cannot iterate, the Object is empty, otherwise not.
          *
-         * @param obj: Object instance to be evaluated
-         * @returns {boolean} Whether the Object is empty or not
          * @memberOf apy.components.CompositeMixin
+         *
+         * @param {Object} obj Object instance to be evaluated
+         *
+         * @return {boolean} Whether the Object is empty or not
          */
         function isEmpty(obj) {
             for (var key in obj) {
@@ -305,18 +324,19 @@
          * Common method which is charge to clean the component inner data.
          * All data-related inner components are collected into an Object instance.
          *
-         * @returns {Object} collected data-related inner components
          * @memberOf apy.components.CompositeMixin
+         *
+         * @return {Object} collected data-related inner components
          */
-        function cleanedData () {
+        function cleanedData() {
             var cleaned = {};
-            this.$components.forEach(function (comp) {
+            this.$components.forEach(function(comp) {
                 var data = comp.cleanedData();
-                if(data || data === false || data === 0) {
+                if (data || data === false || data === 0) {
                     cleaned[comp.$name] = data;
                 }
             });
-            if(isEmpty(cleaned)) {
+            if (isEmpty(cleaned)) {
                 cleaned = undefined;
             }
             return cleaned;
@@ -326,16 +346,17 @@
          * Common method which tells whether the component state is UPDATED or not
          * In other words, whether one of its inner component is UPDATED or not
          *
-         * @returns {boolean} true if UPDATED state
          * @memberOf apy.components.CompositeMixin
+         *
+         * @return {boolean} true if UPDATED state
          */
-        function hasUpdated () {
-            if(this.$selfUpdated) {
+        function hasUpdated() {
+            if (this.$selfUpdated) {
                 return true;
             }
             var updated = false;
-            this.$components.forEach(function (comp) {
-                if(comp.hasUpdated()) {
+            this.$components.forEach(function(comp) {
+                if (comp.hasUpdated()) {
                     updated = true;
                 }
             });
@@ -345,15 +366,16 @@
         /**
          * Common method to definitely set the updated components' values,
          * after a successful Request to the backend has been acknowledged.
-         * `$memo` attribute is overridden by the current `$value`.hasUpdated
+         * `$memo` attribute is overridden by the current `$value`
          *
-         * @returns {this}
          * @memberOf apy.components.CompositeMixin
+         *
+         * @return {apy.components.CompositeMixin} `this`
          */
-        function selfCommit () {
-            this.$components.forEach(function (comp) {
+        function selfCommit() {
+            this.$components.forEach(function(comp) {
                 comp && comp.selfCommit && comp.selfCommit();
-                if(comp.$selfUpdated) {
+                if (comp.$selfUpdated) {
                     comp.$selfUpdated = false;
                 }
             });
@@ -363,34 +385,31 @@
         /**
          * Common Method to allow one component to update itself.
          *
-         * @param update: The update payload (Object)
-         * @param commit: If true, `selfCommit` method is invoked
-         * @returns {this}
          * @memberOf apy.components.CompositeMixin
+         *
+         * @param {Object} update The update payload
+         * @param {boolean} commit If true, `selfCommit` method is invoked
+         *
+         * @return {apy.components.CompositeMixin} `this`
          */
-        function selfUpdate (update, commit) {
+        function selfUpdate(update, commit) {
             var self = this;
             update = update || {};
             commit = commit || false;
             // Copy private properties such as _id, _etag, ...
-            Object.keys(update).forEach(function (name) {
-                if(self.continue(name)) {
+            Object.keys(update).forEach(function(name) {
+                if (self.continue(name)) {
                     self[name] = update[name];
                 }
             });
-            //$apy.forEach(update, function (value, name) {
-            //    if(self.continue(name)) {
-            //        self[name] = value;
-            //    }
-            //});
             // Copy $value in case of embedded resource
-            if(update.hasOwnProperty('$value')) {
+            if (update.hasOwnProperty('$value')) {
                 this.$value = update.$value;
             }
             // Copy data
-            if(update.hasOwnProperty('$components')) {
-                self.$components.forEach(function (comp, index) {
-                    if(update.$components[index]) {
+            if (update.hasOwnProperty('$components')) {
+                self.$components.forEach(function(comp, index) {
+                    if (update.$components[index]) {
                         comp.selfUpdate(update.$components[index], commit);
                     }
                 });
@@ -405,9 +424,12 @@
         }
 
         /**
+         * Composite Component String representation
+         *
          * @override
-         * @returns {*}
          * @memberOf apy.components.CompositeMixin
+         *
+         * @return {string} Composite Component string representation
          */
         function toString() {
             var values = [];
@@ -416,30 +438,30 @@
                 $apy.helpers.$TYPES.DATETIME
             ];
 
-            var filtered = this.$components.filter(function (c) {
+            var filtered = this.$components.filter(function(c) {
                 return c.$required && excludedTypes.indexOf(c.$type) === -1;
             });
 
-            if(!filtered.length) {
-                filtered = this.$components.filter(function (c) {
+            if (!filtered.length) {
+                filtered = this.$components.filter(function(c) {
                     return excludedTypes.indexOf(c.$type) === -1;
                 });
             }
-            filtered.forEach(function (c) {
-                if(c.$value) {
+            filtered.forEach(function(c) {
+                if (c.$value) {
                     var toString = c.toString();
-                    if(toString) {
+                    if (toString) {
                         values.push(toString);
                     }
                 }
             });
-            if(values.length) {
+            if (values.length) {
                 return values.join(', ');
             }
             return '';
         }
 
-        return function () {
+        return function() {
             this.toString = toString;
             this.load = load;
             this._load = _load;
@@ -451,7 +473,5 @@
             this.$$createField = createField;
             return this;
         };
-
     })();
-
-})( apy );
+})(apy);
