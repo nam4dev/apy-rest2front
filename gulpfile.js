e50a859032d8da44d1f77034d56812f3c5926497
Namgyal Brisson <namat4css@gmail.com>
2016-08-23 10:30:06 +0000

    Update gulpfile.js with apy-rest2front configuration paths

--- gulpfile.js
+++ gulpfile.js
@@ -19,42 +19,42 @@ var karma_config = apy_config.gulp.karma;
 var karma_threshold = karma_config.thresholdReporter;
 
 gulp.task('clean', () => {
-    return gulp.src(gp_config.paths.devFiles, {read: false})
-        .pipe(gp_if(!gp_config.dev, gp_clean()))
+    return gulp.src(gp_config.paths.dev.files.src, {read: false})
+        .pipe(gp_if(!gp_config.dev, gp_clean()));
 });
 
 gulp.task('minify-css', () => {
-    return gulp.src(gp_config.paths.cssFiles)
+    return gulp.src(gp_config.paths.css.files.src)
         .pipe(gp_if(gp_config.dev, gp_sourcemaps.init()))
-        .pipe(gp_concat(gp_config.paths.outCssMin))
+        .pipe(gp_concat(gp_config.paths.css.minified))
         .pipe(gp_cssnano(gp_config.minifyCSSOpts))
         .pipe(gp_if(gp_config.dev, gp_sourcemaps.write(gp_config.paths.cwd)))
-        .pipe(gulp.dest(gp_config.paths.appDir + '/scripts'))
+        .pipe(gulp.dest(gp_config.paths.css.dest));
 });
 
 gulp.task('minify-js', () => {
-    return gulp.src(gp_config.paths.jsFiles)
+    return gulp.src(gp_config.paths.js.files.src)
         .pipe(gp_if(gp_config.dev, gp_sourcemaps.init()))
-        .pipe(gp_concat(gp_config.paths.outJsMin))
+        .pipe(gp_concat(gp_config.paths.js.minified))
         .pipe(gp_uglify(gp_config.minifyJSOpts))
         .pipe(gp_if(gp_config.dev, gp_sourcemaps.write(gp_config.paths.cwd)))
-        .pipe(gulp.dest(gp_config.paths.appDir + '/scripts'))
+        .pipe(gulp.dest(gp_config.paths.js.dest));
 });
 
 gulp.task('minify-html', () => {
-    return gulp.src(gp_config.paths.htmlFiles)
+    return gulp.src(gp_config.paths.html.files.src)
         .pipe(gp_htmlmin(gp_config.minifyHTMLOpts))
-        .pipe(gulp.dest(gp_config.paths.appDir))
+        .pipe(gulp.dest(gp_config.paths.appDir));
 });
 
 gulp.task('copy-fonts', () => {
-    return gulp.src(gp_config.paths.fontFiles)
-        .pipe(gulp.dest(gp_config.paths.appDir + '/fonts'))
+    return gulp.src(gp_config.paths.fonts.files.src)
+        .pipe(gulp.dest(gp_config.paths.fonts.dest));
 });
 
 gulp.task('copy-icons', () => {
-    return gulp.src(gp_config.paths.iconFiles)
-        .pipe(gulp.dest(gp_config.paths.appDir))
+    return gulp.src(gp_config.paths.icon.files.src)
+        .pipe(gulp.dest(gp_config.paths.appDir));
 });
 
 gulp.task('prepare', (done) => {
@@ -67,25 +67,26 @@ gulp.task('prepare', (done) => {
     });
 });
 
+gulp.task('copy-doc-design', () => {
+    return gulp.src(gp_config.paths.doc.files.design.src)
+        .pipe(gulp.dest(gp_config.paths.doc.files.design.dest));
+});
+
+gulp.task('copy-doc', ['copy-doc-design'], () => {});
+
 gulp.task('lint', ['prepare'], () => {
     // ESLint ignores files with "node_modules" paths.
     // So, it's best to have gulp ignore the directory as well.
     // Also, Be sure to return the stream from the task;
     // Otherwise, the task may end before the stream has finished.
-    return gulp.src([
-        'app/apy-rest2front/**/*.js',
-        'app/apy-rest2front/**/**/*.js',
-        'app/apy-rest2front/**/**/**/*.js',
-        'app/apy-rest2front/**/**/**/**/*.js'])
+    return gulp.src(gp_config.paths.eslint.src)
         // eslint() attaches the lint output to the "eslint" property
         // of the file object so it can be used by other modules.
-        .pipe(gp_eslint({
-            fix: true
-        }))
+        .pipe(gp_eslint())
         // eslint.format() outputs the lint results to the console.
         // Alternatively use eslint.formatEach() (see Docs).
         .pipe(gp_eslint.format())
-        .pipe(gp_eslint.format('json', fs.createWriteStream(gp_config.paths.outDir + '/lint-report.json')))
+        .pipe(gp_eslint.format('json', fs.createWriteStream(gp_config.paths.eslint.report)))
         // To have the process exit with an error code (1) on
         // lint error, return the stream and pipe to failAfterError last.
         .pipe(gp_eslint.failAfterError());
@@ -104,14 +105,16 @@ gulp.task('test', ['lint'], (done) => {
     // run Karma
     exec('./node_modules/karma/bin/karma start karma.conf.js --single-run', (error, stdout, stderr) => {
         function getResultAsFloat(str) {
+            var pass;
+            var total;
             try {
-                var pass = str.split('/')[0];
-                var total = str.split('/')[1];
+                pass = str.split('/')[0];
+                total = str.split('/')[1];
             } catch(err) {
                 console.log(`Error: ${err}`);
                 return 0.0;
             }
-            return (parseFloat(pass) / parseFloat(total) * 100)
+            return (parseFloat(pass) / parseFloat(total) * 100);
         }
         function getAvgResult(lines, branches, functions, statements) {
             return (
@@ -119,7 +122,7 @@ gulp.task('test', ['lint'], (done) => {
                     (branches || 0.0) +
                     (functions || 0.0) +
                     (statements || 0.0)
-                ) / 4
+                ) / 4;
         }
         var decimals = 2;
         var out = `${stdout}`;
@@ -128,7 +131,7 @@ gulp.task('test', ['lint'], (done) => {
         var branches = getResultAsFloat(re.exec(out) + '');
         var functions = getResultAsFloat(re.exec(out) + '');
         var lines = getResultAsFloat(re.exec(out) + '');
-        var testResultsFile = gp_config.paths.outDir + '/coverage/report.json';
+        var testResultsFile = gp_config.paths.coverage.report;
         var testResults = {
             lines: lines.toFixed(decimals),
             branches: branches.toFixed(decimals),
@@ -160,16 +163,9 @@ gulp.task('test', ['lint'], (done) => {
     });
 });
 
-gulp.task('doc', () => {
-    return gulp.src([
-        'README.md',
-        'app/apy-rest2front/**/*.js',
-        'app/apy-rest2front/**/**/*.js',
-        'app/apy-rest2front/**/**/**/*.js',
-        'app/apy-rest2front/**/**/**/**/*.js',
-        'tests/apy-rest2front/common/*.js'
-    ], {read: false})
-        .pipe(gp_jsdoc3(apy_config.gulp.jsdoc3));
+gulp.task('doc', ['copy-doc'], () => {
+    return gulp.src(gp_config.paths.doc.src, {read: false})
+        .pipe(gp_jsdoc3(gp_config.jsdoc3));
 });
 
 //var es = require('event-stream');
