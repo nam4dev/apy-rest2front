04466c58f17d41706752346121e75d3bb871de66
Namgyal Brisson <namat4css@gmail.com>
2017-10-09 15:03:47 +0200

    gulpfile.js: Add make (settings) task

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,25 @@
-var fs = require('fs');
-var gulp = require('gulp');
-var gp_if = require('gulp-if');
-var mkdirp = require('mkdirp');
-var gp_util = require('gulp-util');
-var gp_clean = require('gulp-clean');
-var gp_jsdoc3 = require('gulp-jsdoc3');
-var gp_concat = require('gulp-concat');
-var gp_rename = require('gulp-rename');
-var gp_uglify = require('gulp-uglify');
-var gp_eslint = require('gulp-eslint');
-var gp_cssnano = require('gulp-cssnano');
-var exec = require('child_process').exec;
-var gp_sourcemaps = require('gulp-sourcemaps');
-var gp_htmlmin = require('gulp-html-minifier');
-var apy_config = require('./apy.conf');
-var gp_config = apy_config.gulp;
-var karma_config = apy_config.gulp.karma;
-var karma_threshold = karma_config.thresholdReporter;
+let fs = require('fs');
+let path = require('path');
+let gulp = require('gulp');
+let gp_if = require('gulp-if');
+let mkdirp = require('mkdirp');
+let gp_util = require('gulp-util');
+let gp_clean = require('gulp-clean');
+let gp_jsdoc3 = require('gulp-jsdoc3');
+let gp_concat = require('gulp-concat');
+let gp_rename = require('gulp-rename');
+let gp_uglify = require('gulp-uglify');
+let gp_eslint = require('gulp-eslint');
+let gp_cssnano = require('gulp-cssnano');
+let exec = require('child_process').exec;
+let gp_sourcemaps = require('gulp-sourcemaps');
+let gp_htmlmin = require('gulp-html-minifier');
+let apy_config = require('./apy.conf');
+let gp_config = apy_config.gulp;
+let karma_config = apy_config.gulp.karma;
+let karma_threshold = karma_config.thresholdReporter;
+
+let default_settings = 'app/apy-rest2front/integration/angular/settings.js';
 
 gulp.task('clean', () => {
     return gulp.src(gp_config.paths.dev.files.src, {read: false})
@@ -33,14 +36,10 @@ gulp.task('minify-css', () => {
 });
 
 gulp.task('minify-js', () => {
-    var customSettings = gp_util.env.settings;
+    let customSettings = gp_util.env.settings;
     if(customSettings) {
-        var index = gp_config.paths.js.files.src.indexOf(
-            'app/apy-rest2front/integration/angular/settings.js'
-        );
-        if (index !== -1) {
-            gp_config.paths.js.files.src[index] = customSettings;
-        }
+        let index = gp_config.paths.js.files.src.indexOf(default_settings);
+        gp_config.paths.js.files.src[index] = customSettings;
     }
     return gulp.src(gp_config.paths.js.files.src)
         .pipe(gp_if(gp_config.dev, gp_sourcemaps.init()))
@@ -119,8 +118,8 @@ gulp.task('test', ['lint'], (done) => {
     // run Karma
     exec('./node_modules/karma/bin/karma start karma.conf.js --single-run', (error, stdout, stderr) => {
         function getResultAsFloat(str) {
-            var pass;
-            var total;
+            let pass;
+            let total;
             try {
                 pass = str.split('/')[0];
                 total = str.split('/')[1];
@@ -138,15 +137,15 @@ gulp.task('test', ['lint'], (done) => {
                     (statements || 0.0)
                 ) / 4;
         }
-        var decimals = 2;
-        var out = `${stdout}`;
-        var re = /(\d+\/\d+)/g;
-        var statements = getResultAsFloat(re.exec(out) + '');
-        var branches = getResultAsFloat(re.exec(out) + '');
-        var functions = getResultAsFloat(re.exec(out) + '');
-        var lines = getResultAsFloat(re.exec(out) + '');
-        var testResultsFile = gp_config.paths.coverage.report;
-        var testResults = {
+        let decimals = 2;
+        let out = `${stdout}`;
+        let re = /(\d+\/\d+)/g;
+        let statements = getResultAsFloat(re.exec(out) + '');
+        let branches = getResultAsFloat(re.exec(out) + '');
+        let functions = getResultAsFloat(re.exec(out) + '');
+        let lines = getResultAsFloat(re.exec(out) + '');
+        let testResultsFile = gp_config.paths.coverage.report;
+        let testResults = {
             lines: lines.toFixed(decimals),
             branches: branches.toFixed(decimals),
             functions: functions.toFixed(decimals),
@@ -161,10 +160,10 @@ gulp.task('test', ['lint'], (done) => {
                 return done(err);
             }
         });
-        var completeStdout = `${stdout}`.replace(
+        let completeStdout = `${stdout}`.replace(
             `\n================================================================================`,
             function injectAverage(){
-                var output = '\n';
+                let output = '\n';
                 output += `Average      : ${testResults.average}% `;
                 output += `( (${testResults.lines}+${testResults.branches}+${testResults.functions}+${testResults.statements})/4 ) `;
                 output += `Threshold : ${testResults.thresholdAverage}%\n`;
@@ -182,51 +181,19 @@ gulp.task('doc', ['copy-doc'], () => {
         .pipe(gp_jsdoc3(gp_config.jsdoc3));
 });
 
-//var es = require('event-stream');
-//var path = require('path');
-//var documentation = require('gulp-documentation');
-//
-//var walk = function(dir) {
-//    var results = [];
-//    var list = fs.readdirSync(dir);
-//    list.forEach(function(file) {
-//        file = dir + '/' + file;
-//        var stat = fs.statSync(file);
-//        if (stat && stat.isDirectory()) {
-//            results = results.concat(walk(file));
-//        }
-//        else {
-//            results.push(file);
-//        }
-//    });
-//    return results
-//};
-
-//gulp.task('docs', function () {
-//    var files = walk('app/apy-rest2front/');
-//    var tasks = [];
-//    files.forEach(function (file) {
-//        var basename = path.basename(file);
-//        var parts = basename.split('.');
-//        var ext = parts.pop();
-//        if(ext === 'js') {
-//            var filename = parts.join('') + '.md';
-//            console.log('filename', filename);
-//            tasks.push(gulp.src(file)
-//                .pipe(documentation({
-//                    format: 'md',
-//                    filename: filename
-//                })).pipe(gulp.dest("build/api")))
-//        }
-//
-//    });
-//    return es.merge(tasks);
-//});
-
 
 // Grouping task units
 gulp.task('copy', ['copy-fonts', 'copy-icons'], () => {});
 gulp.task('minify', ['minify-css', 'minify-js', 'minify-html'], () => {});
-gulp.task('build', ['clean', 'test', 'minify', 'copy', 'doc'], () => {});
+gulp.task('build', ['clean', 'test', 'minify', 'copy', 'doc'], () => {
+    return gulp.src(['build/docs/**/*'], {base: 'build'})
+        .pipe(gulp.dest('.'));
+});
+gulp.task('make', [], () => {
+    let customSettings = gp_util.env.settings;
+    return gulp.src(default_settings)
+        .pipe(gp_rename(path.basename(customSettings)))
+        .pipe(gulp.dest(path.dirname(customSettings)));
+});
 // Default task
 gulp.task('default', ['build'], () => {});
